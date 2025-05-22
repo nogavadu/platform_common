ea7aeaf033541364fefebd952b15e7fdff0306fd
Daniil Mosolov <dkuets@gmail.com>
2025-05-22 21:46:40 +0300

    feat: initial commit

---
+++ pkg/db/db.go
@@ -0,0 +1,63 @@
+package db
+
+import (
+	"context"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+)
+
+type Client interface {
+	DB() DB
+	Close() error
+}
+
+// DB интерфейс для работы с БД
+type DB interface {
+	SQLExecer
+	Pinger
+	Transactor
+	Close()
+}
+
+// Handler - функция, которая выполняется в транзакции
+type Handler func(ctx context.Context) error
+
+// TxManager менеджер транзакций, который выполняет указанный пользователем обработчик в транзакции
+type TxManager interface {
+	ReadCommitted(ctx context.Context, f Handler) error
+}
+
+type Transactor interface {
+	BeginTx(ctx context.Context, txOptions pgx.TxOptions) (pgx.Tx, error)
+}
+
+// Query обертка над запросом, хранящая имя запроса и сам запрос
+// Имя запроса используется для логирования и потенциально может использоваться еще где-то, например, для трейсинга
+type Query struct {
+	Name     string
+	QueryRaw string
+}
+
+// NamedExecer интерфейс для работы с именованными запросами с помощью тегов в структурах
+type NamedExecer interface {
+	ScanOneContext(ctx context.Context, dest interface{}, q Query, args ...interface{}) error
+	ScanAllContext(ctx context.Context, dest interface{}, q Query, args ...interface{}) error
+}
+
+// QueryExecer интерфейс для работы с обычными запросами
+type QueryExecer interface {
+	ExecContext(ctx context.Context, q Query, args ...interface{}) (pgconn.CommandTag, error)
+	QueryContext(ctx context.Context, q Query, args ...interface{}) (pgx.Rows, error)
+	QueryRowContext(ctx context.Context, q Query, args ...interface{}) pgx.Row
+}
+
+// SQLExecer комбинирует NamedExecer и QueryExecer
+type SQLExecer interface {
+	NamedExecer
+	QueryExecer
+}
+
+// Pinger интерфейс для проверки соединения с БД
+type Pinger interface {
+	Ping(ctx context.Context) error
+}
